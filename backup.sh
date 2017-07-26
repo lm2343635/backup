d043c852ecc8219ba97c57a1e0529395dcc9d17a
Meng Li <lm2343635@126.com>
2017-07-27 01:26:36 +0900

    Backup php, nginx conf and database.

---
+++ backup.sh
@@ -0,0 +1,17 @@
+WWWROOT=/data/wwwroot;
+NGINX_CONF=/usr/local/nginx/conf/vhost;
+
+site=$1;
+path=$2;
+username=$3;
+password=$4;
+
+backupPath="$path/$site"
+
+mkdir $backupPath
+echo "Copying source file from $WWWROOT/$site to $backupPath."
+cp -rf "$WWWROOT/$site" $backupPath
+echo "Copying nginx config file from $NGINX_CONF/$site.conf to $backupPath"
+cp "$NGINX_CONF/$site.conf" $backupPath
+echo "Dumping database $site to file ${backupPath}/$site.sql."
+mysqldump -u $username -p$password $site > "${backupPath}/$site.sql"

bca6e98d034d8083e6ad0fe0465a85dd4dc91d24
Meng Li <lm2343635@126.com>
2017-08-29 16:11:28 +0800

    Update recover.sh

--- recover.sh
+++ recover.sh
@@ -2,16 +2,20 @@ WWWROOT=/data/wwwroot;
 NGINX_CONF=/usr/local/nginx/conf/vhost;
 
 path=$1;
-username=$2;
-password=$3;
-
-fullPath=`pwd $path`
-site=${fullPath##*/}
+site=$2;
+username=$3;
+password=$4;
 
 recoverPath="$WWWROOT/$site"
 # Try to delete if recover path is existing.
 rm -rf $recoverPath
 echo "Copying source file from $path/$site to $recoverPath."
 cp -rf "$path/$site" $recoverPath
+chmod -R 755 $recoverPath
+chown -R www:www $recoverPath
+
+echo "Reovering database $site"
+echo "drop database if exists ${site}; create database ${site} character set utf8 collate utf8_general_ci; use ${site}; source ${path}/${site}.sql;" | mysql -u $username -p$password
+
 echo "Copying nginx config file from $path/$site.conf to $NGINX_CONF/$site.conf"
 cp "$path/$site.conf" "$NGINX_CONF/$site.conf"

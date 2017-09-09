26d1dcfb53db4889d51436d379c5635427a91128
root <root@iZ8vb22jlzvz7ta2l7m0b9Z.(none)>
2017-09-09 15:26:59 +0800

    Add recover_all.sh

---
+++ recover_all.sh
@@ -0,0 +1,9 @@
+path=$1;
+
+for file in `ls $path`
+do
+    site="${file%.*}";
+    folder="$path$file";
+    echo "Recovering site $site from database $folder..."
+    sh recover.sh $site $folder
+done

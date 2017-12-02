7355d5c4e19408649d47cf18328172d0ef480821
Meng Li <lm2343635@126.com>
2017-12-02 10:00:04 +0800

    User recover_zip.sh in recover_all.sh

--- recover_all.sh
+++ recover_all.sh
@@ -4,6 +4,6 @@ for file in `ls $path`
 do
     site="${file%.*}";
     folder="$path$file";
-    echo "Recovering site $site from database $folder..."
-    sh recover.sh $site $folder
+    echo "Recovering site $site from $folder."
+    sh recover_zip.sh $site $folder 
 done
